e1879b352f9de5b1f307f763df9fe8643f2990d1
Angelino Valeta <angelinovaleta@gmail.com>
2022-08-03 17:25:23 +0100

    Configure typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ProductsController } from './products.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Angelson@dev2k2',
+      database: 'online_store',
+      "entities": ["dist/**/*.entity{.ts,.js}"],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, ProductsController],
 })
 export class AppModule {}
